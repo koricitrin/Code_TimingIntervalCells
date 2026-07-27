e852e0a419cba6d0e7f6f3d7cb1d8b6902cf6348
koricitrin <55243541+koricitrin@users.noreply.github.com>
2026-07-27 15:43:40 -0400

    Delete PreProcessing/AlignmentCode/RunProcessing2P_imm.m

--- PreProcessing/AlignmentCode/RunProcessing2P_imm.m
+++
@@ -1,6 +0,0 @@
-function RunProcessing2P_imm(BehaviorPath, FileNameBase,mazeSess)
-%% second stage processing
-%% e.g.: RunProcessing2P_imm('Z:\Kori\immobile_2p\anmc156\A156-20230603\A156-20230603-02\','A156-20230603-02',1)
-
-ProcessingAligned2P_imm(BehaviorPath,[FileNameBase '_DataStructure_mazeSection1_TrialType1'],mazeSess);
-
